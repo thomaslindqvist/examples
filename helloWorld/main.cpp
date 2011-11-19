10a42b0b8e35b6ff56a12c4ffc54b38de7a28cc7
Thomas Lindqvist <thomas@famlindqvist.se>
2011-11-19 11:47:24 +0100

    first commit

---
+++ helloWorld/main.cpp
@@ -0,0 +1,11 @@
+//$Id: main.cpp 12 2011-06-11 16:51:37Z home.thomas.lindqvist@gmail.com $
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout<<"Hello World!\n";
+   return 1;
+}
