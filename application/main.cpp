ebb4c42132e4d2776285b502f69d2fec8dc3ce19
Thomas Lindqvist <thomas@famlindqvist.se>
2011-12-06 00:10:05 +0100

    Added application and modifed signals.

---
+++ application/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <iostream>
+#include "signals.h"
+
+
+int main()
+{
+   //Handle signals
+   Signals::enableAll();
+   
+   while(true)
+   {
+
+   }
+
+   return 1;
+}
